a5cc7e27729f7da7baab89dca6401b38a2d0abdb
David Read <davidread1998@gmail.com>
2022-01-28 12:44:57 -0500

    Documentation, add TextView that displays sum of all dice values, pop Snackbar messages when winning and losing conditions are met
    activity_main.xml:
    - Add sum TextView.
    - Add some padding to parent layout.
    - Change id names to be more consistent.
    
    Dice.java:
    - Document this file.
    
    dimens.xml:
    - Add dimen for generic activity padding.
    
    MainActivity.java:
    - Document this activity, move method order around, and refactor names.
    - Change showDice() to updateUI(). It updates the dice user interface and the sum user interface.
    - Define checkForWinConditions() to pop a snackbar when winning conditions are met.
    - Define checkForLosingConditions() to pop a snackbar when losing conditions are met.
    - Call checkForWinningConditions() and checkForLosingConditions() in onFinish() of mTimer.
    
    menu_main.xml:
    - Renamed from "appbar_menu.xml".
    
    strings.xml:
    - Group similar strings together.
    - Add sum label string.
    - Add snackbar message strings.

--- app/src/main/java/com/davidread/diceroller/Dice.java
+++ app/src/main/java/com/davidread/diceroller/Dice.java
@@ -1,26 +1,53 @@
 package com.davidread.diceroller;
 
-import java.util.Calendar;
 import java.util.Random;
 
+/**
+ * {@link Dice} is a model class for a six-sided die.
+ */
 public class Dice {
 
+    /**
+     * Int constants for this die's largest and smallest possible value.
+     */
     public static int LARGEST_NUM = 6;
     public static int SMALLEST_NUM = 1;
 
+    /**
+     * Int representing the value of this die.
+     */
     private int mNumber = SMALLEST_NUM;
-    private int mImageId;
-    private Random mRandomGenerator;
 
+    /**
+     * Int representing the ID of the image resource that corresponds to this die's value.
+     */
+    private int mImageId;
+
+    /**
+     * {@link Random} used to generate random values for this die.
+     */
+    private final Random mRandomGenerator;
+
+    /**
+     * Constructs a new {@link Dice}.
+     *
+     * @param number Value to initialize on this die.
+     */
     public Dice(int number) {
         setNumber(number);
-        mRandomGenerator = new Random(Calendar.getInstance().getTimeInMillis());
+        mRandomGenerator = new Random();
     }
 
+    /**
+     * Returns the value of this die.
+     */
     public int getNumber() {
         return mNumber;
     }
 
+    /**
+     * Sets the value of this die.
+     */
     public void setNumber(int number) {
         if (number >= SMALLEST_NUM && number <= LARGEST_NUM) {
             mNumber = number;
@@ -47,18 +74,30 @@ public class Dice {
         }
     }
 
+    /**
+     * Returns an ID for the image resource corresponding to this die's value.
+     */
     public int getImageId() {
         return mImageId;
     }
 
+    /**
+     * Increments this die's value by one.
+     */
     public void addOne() {
         setNumber(mNumber + 1);
     }
 
+    /**
+     * Decrements this die's value by one.
+     */
     public void subtractOne() {
         setNumber(mNumber - 1);
     }
 
+    /**
+     * Assigns a random value to this die.
+     */
     public void roll() {
         setNumber(mRandomGenerator.nextInt(LARGEST_NUM) + 1);
     }
--- app/src/main/java/com/davidread/diceroller/MainActivity.java
+++ app/src/main/java/com/davidread/diceroller/MainActivity.java
@@ -7,72 +7,146 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
+
+/**
+ * {@link MainActivity} represents a user interface with three dice that may be rolled by the user.
+ * Controls to change the number of dice, to roll the dice, and stop rolling the dice are in this
+ * activity's action bar.
+ */
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * Int constant representing the maximum number of dice allowed to be shown on screen.
+     */
     public static final int MAX_DICE = 3;
 
+    /**
+     * Int representing the number of dice visible on screen.
+     */
     private int mVisibleDice;
+
+    /**
+     * Array of {@link Dice} to model dice values and random rolls.
+     */
     private Dice[] mDice;
+
+    /**
+     * Array of {@link ImageView} that hold the dice image drawables in the user interface.
+     */
     private ImageView[] mDiceImageViews;
+
+    /**
+     * Global reference to this activity's {@link Menu}.
+     */
     private Menu mMenu;
+
+    /**
+     * {@link CountDownTimer} used to re-roll die a few times a second to give a dice roll
+     * animation.
+     */
     private CountDownTimer mTimer;
 
+    /**
+     * Int representing the sum of the values of the dice visible on screen.
+     */
+    private int sum;
+
+    /**
+     * {@link TextView} to hold the sum in the user interface.
+     */
+    private TextView sumTextView;
+
+    /**
+     * Callback method invoked when the activity is created. It initializes member variables and
+     * initializes the user interface.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Create an array of Dice
+        // Initialize mDice array.
         mDice = new Dice[MAX_DICE];
         for (int i = 0; i < MAX_DICE; i++) {
             mDice[i] = new Dice(i + 1);
         }
 
-        // Create an array of ImageViews
+        // Initialize mDiceImageViews array.
         mDiceImageViews = new ImageView[MAX_DICE];
-        mDiceImageViews[0] = findViewById(R.id.dice1);
-        mDiceImageViews[1] = findViewById(R.id.dice2);
-        mDiceImageViews[2] = findViewById(R.id.dice3);
+        mDiceImageViews[0] = findViewById(R.id.dice_1);
+        mDiceImageViews[1] = findViewById(R.id.dice_2);
+        mDiceImageViews[2] = findViewById(R.id.dice_3);
 
-        // All dice are initially visible
+        // Set mVisibleDice to MAX_DICE since all dice are initially visible.
         mVisibleDice = MAX_DICE;
 
-        showDice();
+        // Initialize sum TextView.
+        sumTextView = findViewById(R.id.sum_text_view);
+
+        // Call updateUI() to initialize user interface.
+        updateUI();
     }
 
+    /**
+     * Callback method invoked when the action bar is created.
+     *
+     * @param menu {@link Menu} where the action bar menu should be inflated.
+     * @return Whether the action bar should be displayed.
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.appbar_menu, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         mMenu = menu;
         return super.onCreateOptionsMenu(menu);
     }
 
+    /**
+     * Callback method invoked when an action bar button is selected.
+     *
+     * @param item {@link MenuItem} that is invoking this method.
+     * @return False to allow normal processing to proceed. True to consume it here.
+     */
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
-        // Determine which menu option was chosen
+        // When "One" is selected, only show one die on screen.
         if (item.getItemId() == R.id.action_one) {
             changeDiceVisibility(1);
-            showDice();
+            updateUI();
             return true;
-        } else if (item.getItemId() == R.id.action_two) {
+        }
+
+        // When "Two" is selected, only show two dice on screen.
+        else if (item.getItemId() == R.id.action_two) {
             changeDiceVisibility(2);
-            showDice();
+            updateUI();
             return true;
-        } else if (item.getItemId() == R.id.action_three) {
+        }
+
+        // When "Three" is selected, only show three dice on screen.
+        else if (item.getItemId() == R.id.action_three) {
             changeDiceVisibility(3);
-            showDice();
+            updateUI();
             return true;
-        } else if (item.getItemId() == R.id.action_stop) {
+        }
+
+        // When "Stop" is selected, stop rolling dice and hide this action bar button.
+        else if (item.getItemId() == R.id.action_stop) {
             mTimer.cancel();
             item.setVisible(false);
             return true;
-        } else if (item.getItemId() == R.id.action_roll) {
+        }
+
+        // When "Roll" is selected, roll the dice.
+        else if (item.getItemId() == R.id.action_roll) {
             rollDice();
             return true;
         }
@@ -80,47 +154,106 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * Changes the number of dice shown on screen to the passed argument.
+     *
+     * @param numVisible The number of dice to show on screen.
+     */
     private void changeDiceVisibility(int numVisible) {
+
+        // Update mVisibleDice.
         mVisibleDice = numVisible;
 
-        // Make dice visible
+        // Make dice visible.
         for (int i = 0; i < numVisible; i++) {
             mDiceImageViews[i].setVisibility(View.VISIBLE);
         }
 
-        // Hide remaining dice
+        // Hide remaining dice.
         for (int i = numVisible; i < MAX_DICE; i++) {
             mDiceImageViews[i].setVisibility(View.GONE);
         }
     }
 
-    private void showDice() {
-        // Display only the number of dice visible
-        for (int i = 0; i < mVisibleDice; i++) {
-            Drawable diceDrawable = ContextCompat.getDrawable(this, mDice[i].getImageId());
-            mDiceImageViews[i].setImageDrawable(diceDrawable);
-            mDiceImageViews[i].setContentDescription(Integer.toString(mDice[i].getNumber()));
-        }
-    }
-
+    /**
+     * Rolls all dice on screen with a nice animation.
+     */
     private void rollDice() {
+
+        // Show "Stop" action bar button.
         mMenu.findItem(R.id.action_stop).setVisible(true);
 
+        // Stop mTimer if it is already running.
         if (mTimer != null) {
             mTimer.cancel();
         }
 
+        // Initialize mTimer to call roll() on all Dice objects to display a dice roll animation.
         mTimer = new CountDownTimer(2000, 100) {
             public void onTick(long millisUntilFinished) {
                 for (int i = 0; i < mVisibleDice; i++) {
                     mDice[i].roll();
                 }
-                showDice();
+                updateUI();
             }
 
             public void onFinish() {
                 mMenu.findItem(R.id.action_stop).setVisible(false);
+                checkForWinConditions();
+                checkForLoseConditions();
             }
         }.start();
+    }
+
+    /**
+     * Updates the user of interface of this activity to match the logic of {@link #mDice}.
+     */
+    private void updateUI() {
+
+        // Display only the number of dice visible.
+        for (int i = 0; i < mVisibleDice; i++) {
+            Drawable diceDrawable = ContextCompat.getDrawable(this, mDice[i].getImageId());
+            mDiceImageViews[i].setImageDrawable(diceDrawable);
+            mDiceImageViews[i].setContentDescription(Integer.toString(mDice[i].getNumber()));
+        }
+
+        // Display the sum.
+        sum = 0;
+        switch (mVisibleDice) {
+            case 1:
+                sum = mDice[0].getNumber();
+                break;
+            case 2:
+                sum = mDice[0].getNumber() + mDice[1].getNumber();
+                break;
+            case 3:
+                sum = mDice[0].getNumber() + mDice[1].getNumber() + mDice[2].getNumber();
+                break;
+        }
+        sumTextView.setText(getString(R.string.sum_label, sum));
+    }
+
+    /**
+     * Checks {@link #sum} for winning conditions. If one is detected, a {@link Snackbar} is popped
+     * on screen.
+     */
+    private void checkForWinConditions() {
+        if ((mVisibleDice == 2) && (sum == 7 || sum == 11)) {
+            Snackbar.make(sumTextView, R.string.win_message, BaseTransientBottomBar.LENGTH_SHORT).show();
+        } else if ((mVisibleDice == 3) && (sum % 7 == 0 || sum % 11 == 0)) {
+            Snackbar.make(sumTextView, R.string.win_message, BaseTransientBottomBar.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * Checks {@link #sum} for losing conditions. If one is detected, a {@link Snackbar} is popped
+     * on screen.
+     */
+    private void checkForLoseConditions() {
+        if ((mVisibleDice == 2) && (sum == 2 || sum == 12)) {
+            Snackbar.make(sumTextView, R.string.lose_message, BaseTransientBottomBar.LENGTH_SHORT).show();
+        } else if ((mVisibleDice == 3) && (sum == 18 || sum == 3)) {
+            Snackbar.make(sumTextView, R.string.lose_message, BaseTransientBottomBar.LENGTH_SHORT).show();
+        }
     }
 }
